a1d4bbb21562ea894b0a8568f0a82b294c1e7440
jtnoble <jtnoble321@gmail.com>
2024-10-30 18:39:26 -0500

    beamsmasher puzzle selection added

--- src/components/beamsmasher_math_puzzle.tsx
+++ src/components/beamsmasher_math_puzzle.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import bs_0 from '../assets/beamsmasher_0.png'
 import bs_10 from '../assets/beamsmasher_10.png'
 import bs_11 from '../assets/beamsmasher_11.png'
@@ -6,74 +7,97 @@ import bs_21 from '../assets/beamsmasher_21.png'
 import bs_22 from '../assets/beamsmasher_22.png'
 import styles from '../styles/beamsmasher_math_puzzle.module.css'
 
-let x = 0;
-let y = 0;
-let z = 0;
-
 const data = [
-    {image: bs_0, value: 0},
-    {image: bs_11, value: 11},
-    {image: bs_10, value: 10},
-    {image: bs_22, value: 22},
-    {image: bs_21, value: 21},
-    {image: bs_20, value: 20}
+    { image: bs_0, value: 0 },
+    { image: bs_11, value: 11 },
+    { image: bs_10, value: 10 },
+    { image: bs_22, value: 22 },
+    { image: bs_21, value: 21 },
+    { image: bs_20, value: 20 }
 ]
 
-const setLetter = (letter: String, index: any) => {
-    switch (letter) {
-        case 'x':
-            x = data[index].value;
-            break;
-        case 'y':
-            y = data[index].value;
-            break;
-        case 'z':
-            z = data[index].value;
-            break;
-        default:
-            console.log("An error has occurred...")
-    }
+const PuzzleItem: React.FC<any> = ({ index, rowCharacter, isSelected, onSelect }: any) => {
+    const handleClick = () => {
+        onSelect(index);
+    };
+
+    return (
+        <td>
+            <div className={styles.imgOverlay}>
+                <img
+                    src={data[index].image}
+                    alt={data[index].value.toString()}
+                    onClick={handleClick}
+                    className={isSelected ? styles.selected : ''}
+                />
+                {isSelected && (
+                    <div className={styles.selectedOverlay}>
+                        <span>Selected</span>
+                    </div>
+                )}
+            </div>
+        </td>
+    );
+};
+
+
+const PuzzleRow: React.FC<any> = ({ rowCharacter }: any) => {
+    const [selectedIndex, setSelectedIndex] = useState<number | null>(null);
+
+    const handleSelect = (index: number) => {
+        setSelectedIndex(index);
+    };
+
+    const rowElements = data.map((item, index) => (
+        <PuzzleItem
+            key={index}
+            index={index}
+            rowCharacter={rowCharacter}
+            isSelected={selectedIndex === index}
+            onSelect={handleSelect}
+        />
+    ));
+
+    return (
+        <tr>
+            <td>{rowCharacter}: </td>
+            {rowElements}
+        </tr>
+    );
 }
 
 const BeamsmasherMathPuzzle: React.FC = () => {
-    
+    const [xSelected, setXSelected] = useState<number | null>(null);
+    const [ySelected, setYSelected] = useState<number | null>(null);
+    const [zSelected, setZSelected] = useState<number | null>(null);
+
+    const puzzleRows = [
+        <PuzzleRow
+            rowCharacter="X"
+            onSelect={(index: number) => setXSelected(index)}
+        />,
+        <PuzzleRow
+            rowCharacter="Y"
+            onSelect={(index: number) => setYSelected(index)}
+        />,
+        <PuzzleRow
+            rowCharacter="Z"
+            onSelect={(index: number) => setZSelected(index)}
+        />,
+    ];
+
+    const xOutput = xSelected !== null ? data[xSelected].value : 0;
+    const yOutput = ySelected !== null ? data[ySelected].value : 0;
+    const zOutput = zSelected !== null ? data[zSelected].value : 0;
+
     return (
         <>
-        <table className={styles.table}>
-            <tbody>
-            <tr>
-                <td>X: </td>
-                <td><div className={styles.imgOverlay}><img src={data[0].image} alt={data[0].value.toString()} onClick={() => setLetter('x', 0)}/></div></td>
-                <td><img src={data[1].image} alt={data[0].value.toString()} /></td>
-                <td><img src={data[2].image} alt={data[0].value.toString()} /></td>
-                <td><img src={data[3].image} alt={data[0].value.toString()} /></td>
-                <td><img src={data[4].image} alt={data[0].value.toString()} /></td>
-                <td><img src={data[5].image} alt={data[0].value.toString()} /></td>
-            </tr>
-            
-            <tr>
-                <td>Y: </td>
-                <td><img src={data[0].image} alt={data[0].value.toString()} /></td>
-                <td><img src={data[1].image} alt={data[0].value.toString()} /></td>
-                <td><img src={data[2].image} alt={data[0].value.toString()} /></td>
-                <td><img src={data[3].image} alt={data[0].value.toString()} /></td>
-                <td><img src={data[4].image} alt={data[0].value.toString()} /></td>
-                <td><img src={data[5].image} alt={data[0].value.toString()} /></td>
-            </tr>
-            <tr>
-                <td>Z: </td>
-                <td><img src={data[0].image} alt={data[0].value.toString()} /></td>
-                <td><img src={data[1].image} alt={data[0].value.toString()} /></td>
-                <td><img src={data[2].image} alt={data[0].value.toString()} /></td>
-                <td><img src={data[3].image} alt={data[0].value.toString()} /></td>
-                <td><img src={data[4].image} alt={data[0].value.toString()} /></td>
-                <td><img src={data[5].image} alt={data[0].value.toString()} /></td>
-            </tr>
-            </tbody>
-        </table>
-        <p>Output: {x}, {y}, {z}</p>
+            <table className={styles.table}>
+                <tbody>{puzzleRows}</tbody>
+            </table>
+            <p>Output: {xOutput}, {yOutput}, {zOutput}</p>
         </>
-    )
+    );
 }
 
 export default BeamsmasherMathPuzzle;
